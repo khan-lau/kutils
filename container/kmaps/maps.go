fdbe4a538f2b15389ea467bc48feb45825817f3a
khan.lau <liukun@dtxytech.com>
2024-09-23 09:59:00 +0800

    release v0.3.0 1. 新增kmaps接口 2. files.ReadLinesWithBufferSize 接口bugfix

--- container/kmaps/maps.go
+++ container/kmaps/maps.go
@@ -5,6 +5,24 @@ type AnyMap[K comparable, V any] map[K]V
 
 type ComparableMap[K comparable, V comparable] map[K]V
 
+// 获取map中的所有key
+func Keys[K comparable, V any](m AnyMap[K, V]) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// 获取map中的所有Values
+func Values[K comparable, V any](m AnyMap[K, V]) []V {
+	values := make([]V, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	return values
+}
+
 // 判断map中对应的key是否存在
 func HasKey[K comparable, V any](m AnyMap[K, V], key K) bool {
 	if _, ok := m[key]; ok {
@@ -23,9 +41,52 @@ func FindValue[K comparable, V comparable](m ComparableMap[K, V], val V) (*K, bo
 	return nil, false
 }
 
+// Equal reports whether two maps contain the same key/value pairs.
+// Values are compared using ==.
+func Equal[K, V comparable](m1 AnyMap[K, V], m2 AnyMap[K, V]) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+	for k, v1 := range m1 {
+		if v2, ok := m2[k]; !ok || v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+// EqualFunc is like Equal, but compares values using eq.
+// Keys are still compared with ==.
+func EqualFunc[K comparable, V any](m1 AnyMap[K, V], m2 AnyMap[K, V], eq func(V, V) bool) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+	for k, v1 := range m1 {
+		if v2, ok := m2[k]; !ok || !eq(v1, v2) {
+			return false
+		}
+	}
+	return true
+}
+
 // 清除 map中的所有元素
 func Clear[K comparable, V any](m AnyMap[K, V]) {
 	for k := range m {
 		delete(m, k)
+	}
+}
+
+func Copy[K comparable, V any](dst AnyMap[K, V], src AnyMap[K, V]) {
+	for k, v := range src {
+		dst[k] = v
+	}
+}
+
+// DeleteFunc deletes any key/value pairs from m for which del returns true.
+func DeleteFunc[K comparable, V any](m AnyMap[K, V], del func(K, V) bool) {
+	for k, v := range m {
+		if del(k, v) {
+			delete(m, k)
+		}
 	}
 }
--- filesystem/files.go
+++ filesystem/files.go
@@ -8,6 +8,10 @@ import (
 )
 
 func ReadFile(filePath string) (*klists.KList[string], error) {
+	return ReadLinesWithBufferSize(filePath, 64*1024)
+}
+
+func ReadLinesDefaultBufferSize(filePath string) (*klists.KList[string], error) {
 	// 按行读文件
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -16,13 +20,68 @@ func ReadFile(filePath string) (*klists.KList[string], error) {
 	defer file.Close()
 
 	lineList := klists.New[string]()
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file) // 默认缓冲区大小为64KB
 	for scanner.Scan() {
 		lineList.PushBack(scanner.Text())
 	}
 
 	if err = scanner.Err(); err != nil {
 		return nil, err
+	}
+
+	return lineList, nil
+}
+
+func ReadLines(filePath string) (*klists.KList[string], error) {
+	// 按行读文件
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	lineList := klists.New[string]()
+
+	reader := bufio.NewReaderSize(file, 10*1024*1024) //缓冲区10M
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+		lineList.PushBack(string(line))
+	}
+
+	return lineList, nil
+}
+
+func ReadLinesWithBufferSize(filePath string, bufferSize int) (*klists.KList[string], error) {
+	// 按行读文件
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	lineList := klists.New[string]()
+	if bufferSize < 1 {
+		bufferSize = 4 * 1024
+	}
+
+	reader := bufio.NewReaderSize(file, bufferSize) //缓冲区4k
+	// for {
+	// 	line, _, err := reader.ReadLine()
+	// 	if err != nil {
+	// 		break
+	// 	}
+	// 	lineList.PushBack(string(line))
+	// }
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil && len(line) < 1 {
+			break
+		}
+		// line = kstrings.TrimSpace(line)
+		lineList.PushBack(line)
 	}
 
 	return lineList, nil
